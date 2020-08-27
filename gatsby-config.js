9b22c857786dd50c833318619a28c4407345561f
Tiago Vaz <tiago.vaz@hotmail.com>
2020-08-27 20:46:16 -0300

    Cleanup default files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `My Blog`,
+    description: `A blog about frontend development. Create for studies reasons`,
+    author: `@TiagoVaz`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
