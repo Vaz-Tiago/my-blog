5877b228e1a8035a9247cd6a7c2220fbbdc8e165
Tiago Vaz <tiago.vaz@hotmail.com>
2020-08-27 22:08:00 -0300

    get data with graphql and useQueryHook

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,34 +1,57 @@
 import React from 'react'
 
 // Static query for get data from project
-import {StaticQuery, graphql} from 'gatsby'
+import { useStaticQuery, graphql } from 'gatsby'
 
-const Profile = () => (
-  <StaticQuery 
-    query={graphql`
-      query MySiteMetada {
-        site {
-          siteMetadata {
-            author
-            description
-            position
-            title
-          }
+const Profile = () => {
+  const { 
+    site: {
+      siteMetadata: {title, position, description, author}
+    }
+  } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata{
+          author
+          title
+          position
+          description
         }
       }
-    `}
+    }
+  `)
 
-    render={({site: {
-      siteMetadata: {title, position, description, author}
-    }}) => (
-      <div className="Profile-wrapper">
-        <h1>{title}</h1>
-        <h2>{position}</h2>
-        <p>{description}</p>
-        <p>{author}</p>
-      </div>
-    )}
-  />
-)
+  return (
+    <div className="Profile-wrapper">
+      <h1>{title}</h1>
+      <h2>{position}</h2>
+      <p>{description}</p>
+      <p>{author}</p>
+  </div>
+  )
+}
+
+// const Profile = () => (
+//   <StaticQuery 
+//     query={graphql`
+//       query MySiteMetada {
+//         site {
+//           siteMetadata {
+//             author
+//             description
+//             position
+//             title
+//           }
+//         }
+//       }
+//     `}
+
+//     render={({site: {
+//       siteMetadata: {title, position, description, author}
+//     }}) => (
+
+//     )}
+//   />
+// )
 
 export default Profile;
