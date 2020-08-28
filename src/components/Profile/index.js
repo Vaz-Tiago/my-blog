58a6209957d8c0c64caf6f60007623d3851f5554
Tiago Vaz <tiago.vaz@hotmail.com>
2020-08-27 21:18:00 -0300

    Profile and Layout

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Tiago Vaz</h1>
+    <h2>Web Developer</h2>
+    <p>Desenvolvedor</p>
+  </div>
+)
+
+export default Profile;
