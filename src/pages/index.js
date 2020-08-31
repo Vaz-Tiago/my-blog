a63ca17cd88f07e886b401bb7713b76ca48d6d5f
Tiago Vaz <tiago.vaz@hotmail.com>
2020-08-31 09:08:35 -0300

    getting the markdown data with graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,57 @@
 import React from "react"
+import { useStaticQuery, graphql } from 'gatsby'
 
 import SEO from "../components/seo"
 import Layout from "../components/Layout"
 import PostItem from '../components/PostItem'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
+const IndexPage = () => {
 
-    <PostItem
-      slug='/about'
-      background='red'
-      category='misc'
-      date='31/08/2020'
-      timeToRead='5'
-      title='Say no to Medium'
-      description='Some reasons to leave the medium and build your own platform'
-    />
-    
-  </Layout>
-)
+  const {allMarkdownRemark} = useStaticQuery(
+    graphql`
+    query PostList {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              background
+              category
+              date(formatString: "DD [de] MMMM [de] YYYY", locale: "pt-br")
+              description
+              title
+            }
+            timeToRead
+          }
+        }
+      }
+    }
+    `
+  )
+
+  const postList = allMarkdownRemark.edges
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {postList.map(({node: {
+          frontmatter: {background, category, date, description, title},
+          timeToRead
+        }
+        }) => (
+          <PostItem
+            slug='/about'
+            background={background}
+            category={category}
+            date={date}
+            timeToRead={timeToRead}
+            title={title}
+            description={description}
+          />
+        )
+      )}
+      
+    </Layout>
+  )
+}
 
 export default IndexPage
